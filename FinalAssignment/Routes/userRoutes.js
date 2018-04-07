443f1290c2b63173b0603948a4a6dc692454d991
Nathan <nd14127@my.bristol.ac.uk>
2018-04-07 11:00:06 +0100

    two button bois

--- FinalAssignment/Routes/userRoutes.js
+++ FinalAssignment/Routes/userRoutes.js
@@ -4,13 +4,21 @@ const publicPath = __dirname + '/../Public/';
 
 module.exports = function(app){
     var path = require('path');
-    
-
-
+    //html files
     app.get('/', function(req, res){
         res.sendFile(path.resolve(publicPath + 'home.html'));
     });
 
+    app.get('/page2', function(req, res){
+        res.sendFile(path.resolve(publicPath + 'page2.html'));
+    });
+
+    //css files
+    app.get('/home.css', function(req, res) {
+        res.sendFile(path.resolve(publicRes + 'home.css'));
+    });
+
+    //js files
     app.get('/Resources/home.js', function(req, res) {
         res.sendFile(path.resolve(publicRes + 'home.js'));
     });
