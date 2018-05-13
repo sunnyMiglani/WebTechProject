8ea497e3856b11267c6c848de4c3bc08c30fbd3b
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2018-05-13 19:08:21 +0100

    currently hides submit button until correct, ENTER still works

--- FinalAssignment/Public/Resources/Javascript/login.js
+++ FinalAssignment/Public/Resources/Javascript/login.js
@@ -8,6 +8,14 @@ var number = document.getElementById("number");
 var length = document.getElementById("length");
 var both = document.getElementById("both");
 
+var subButton = document.getElementById("subButton");
+
+subButton.style.visibility = "hidden";
+
+var numbCorrect = false;
+var lengthCorrect = false;
+var bothCorrect = false;
+
 if(firstPass === null || both === null || length === null || number === null ){
   console.log("CANNOT FIND THE CORRECT ELEMENT IN LOGIN.JS");
 }
@@ -31,30 +39,41 @@ function hideRequirements() {
     document.getElementById("requirements").style.display = "none";
 };
 
+
+
 function validate() {
     // Validate numbers
     var numbers = /[0-9]/g;
     if(firstPass.value.match(numbers)) {  
       number.classList.remove("invalid");
       number.classList.add("valid");
+      numbCorrect = true;
     } else {
       number.classList.remove("valid");
       number.classList.add("invalid");
+      numbCorrect = false;
     }
     // Validate Length
   if(firstPass.value.length >= 8) {
       length.classList.remove("invalid");
       length.classList.add("valid");
+      lengthCorrect = true;
     } else {
       length.classList.remove("valid");
       length.classList.add("invalid");
+      lengthCorrect = false;
     }
   if(secondPass.value === firstPass.value && secondPass.value.length > 0 ){
       both.classList.remove("invalid");
       both.classList.add("valid");
+      bothCorrect = true;
     } else {
       both.classList.remove("valid");
       both.classList.add("invalid");
+      bothCorrect = false;
+    }  
+  if(numbCorrect === true && lengthCorrect === true && bothCorrect === true){
+      subButton.style.visibility = "visible";
     }
   }
 }
