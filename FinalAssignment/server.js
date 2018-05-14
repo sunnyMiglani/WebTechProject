7ffa51f8cc8aca2dd51017d14ffda68e3dee3aeb
MayorOfAlbuquerque <nd14127@my.bristol.ac.uk>
2018-05-14 15:15:17 +0100

    merge

--- FinalAssignment/server.js
+++ FinalAssignment/server.js
@@ -10,8 +10,8 @@ const configureExpress = require('./Configure/express.js');
 //pass db down to be used with routing
 const apps = configureExpress(db);
 
-const httpApp = apps[1];
 const httpsApp = apps[0];
+const httpApp = apps[1];
 
 httpsApp.listen(3000, function() {
     console.log('Example app listening on port 3000!');
