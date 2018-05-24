8cafe789dc26081043841e4cf3872d6876487d9e
Sunny Miglani <sunny.miglani.2015@my.bristol.ac.uk>
2018-05-24 12:01:45 +0100

    before shit

--- FinalAssignment/Routes/userRoutes.js
+++ FinalAssignment/Routes/userRoutes.js
@@ -73,7 +73,7 @@ module.exports = function(app, db, hashPass) {
                                     console.log(JSON.stringify(bills));
                                     var normalJSON = JSONForVariables(req, 1); 
                                     normalJSON.dashView = ['partials/shopping.ejs', 'partials/house_members.ejs', 'partials/bills.ejs'];
-                                    normalJSON.cssFiles = ["shopping.css", "house_members.css"];
+                                    normalJSON.cssFiles = ["shopping.css", "house_members.css", "bills.css"];
                                     normalJSON.javaScriptFiles = ["shopping.js"];
                                     normalJSON.shopping = JSON.parse(sList[0].sl);
                                     normalJSON.houseMembers = houseMembers;
